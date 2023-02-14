d1a9546d734c237384f23c86db8b8281f8d08a10
Marlon Harrison <marlon@ionic.io>
2023-02-14 12:38:51 -0600

    add CORS/ cookieParser() to main

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,14 +5,20 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import cookieParser from 'cookie-parser';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
+  app.enableCors({
+    credentials:true,
+    origin: ['capacitor://localhost', 'http://localhost:8100', process.env.CLIENT_URL, 'localhost']
+  });
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
