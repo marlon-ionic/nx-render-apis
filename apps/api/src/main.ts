4f847241bac76100baae50471ead017328203a50
Marlon Harrison <marlon@ionic.io>
2023-02-14 13:46:33 -0600

    cors all

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -10,15 +10,15 @@ import cookieParser from 'cookie-parser';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {cors: true});
   app.use(cookieParser());
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
-  app.enableCors({
-    credentials:true,
-    origin: ['capacitor://localhost', 'http://localhost', 'http://localhost:8100', process.env.CLIENT_URL, 'localhost']
-  });
+  // app.enableCors({
+  //   credentials:true,
+  //   origin: ['capacitor://localhost', 'http://localhost', 'http://localhost:8100', process.env.CLIENT_URL, 'localhost']
+  // });
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
