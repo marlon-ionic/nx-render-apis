395af196d5b763de1d800863f856951192014d56
Marlon Harrison <marlon@ionic.io>
2023-02-15 00:49:43 -0600

    add static merchantid.txt

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { UploadModule } from '../upload';
 import { UserAgentModule } from '../user-agent/user-agent.module';
 
@@ -6,7 +8,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [UploadModule, UserAgentModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, 'assets'),
+      serveStaticOptions: {
+        dotfiles: 'allow',
+        index: false
+      }
+    }),
+    UploadModule, UserAgentModule],
   controllers: [AppController],
   providers: [AppService],
 })
